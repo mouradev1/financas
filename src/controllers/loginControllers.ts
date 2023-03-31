b74b77b2a9f4bf3ab9341a60a07464a8462fe4af
Moura <mouralol23@gmail.com>
2023-03-31 18:49:47 -0300

    M- ajustes

--- src/controllers/loginControllers.ts
+++ src/controllers/loginControllers.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-
+import { conn } from '../config/config';
 class loginControllers {
-
+    async auth(req: Request, res: Response){
+        const { name, password } = req.body;
+        
+    }
 }
+
+export { loginControllers }
--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from "express";
+import { conn } from '../config/config';
 
 class UsersControllers {
-
+    async createUser(req: Request, res: Response) {
+        const { name, password, nivel } = req.body;
+        try {
+            const user = conn.users.create({
+                data: {
+                    name,
+                    password,
+                    nivel
+                }
+            })
+            if(await user){
+                
+            }
+        } catch (e) {
+            res.status(500).json({ status: "Algo deu errado" })
+        }
+    }
 }
+
+export { UsersControllers }
